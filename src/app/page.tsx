941466bea3835d1d5a509d5a37c2d642ab4abf73
ITHPARK <ithpark1035@gmail.com>
2024-10-28 17:36:13 +0900

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image'
+import styles from './page.module.css'
 
-export default function Home() {
+const Home = () => {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -91,5 +91,7 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  );
+  )
 }
+
+export default Home
